05986ff87a49f049be38376d3af770ec5ac74bfc
Pedro Henrique <pedrohrezendeb07@gmail.com>
2025-02-20 23:12:02 -0300

    criando a lógica e iniciando a estilização

--- script.js
+++ script.js
@@ -1,9 +1,79 @@
 const app = document.getElementById('app');
+const users = [
+    {
+        email: 'test@test.com',
+        phone: '123456789',
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: 'tust@tust.com',
+        phone: '987654321',
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'tost@tost.com',
+        phone: '987654321',
+        ref: 300,
+        refBy: 200
+    }
+];
+
+const getUser = (userData) => {
+    return users.find((user) => {
+        return user.email == userData.email;
+    })
+};
+
+const getTotalSubscribers = (userData) =>{
+    const subs = users.filter((user) => {
+        return user.refBy == userData.ref;
+     })
+     return subs.length;
+};
+
+const showInvite = (userData) => {
+    app.innerHTML = `
+        <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled/>
+        <div id="stats">
+            <h4>
+                ${getTotalSubscribers(userData)}
+            </h4>
+            <p>
+                Inscrições Feitas
+            </p>
+        </div>
+    `
+};
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 4000),
+        refBy: 100
+    };
+
+    users.push(newUser);
+    console.log(users);
+    return newUser;
+};
 
 const formAction = () => {
     const form = document.getElementById('form');
     form.onsubmit = (event) => {
         event.preventDefault();
+        const formData = new FormData(form);
+        const userData = {
+            email:formData.get('email'),
+            phone:formData.get('phone')
+        };
+        const user = getUser(userData);
+        if(user) {
+            showInvite(user);
+        } else {
+            const newUser = saveUser(userData);
+            showInvite(newUser);
+        };
      }
 }
 
@@ -24,3 +94,7 @@ const startApp = () => {
  }
 
 startApp();
+
+document.getElementById('logo').onclick = () => {
+    startApp();
+}
